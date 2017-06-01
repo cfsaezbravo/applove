f3af3e2550d5c75ec7d726816e5d999eb3dbbf7c
Camila Sáez <cfsaezbravo@gmail.com>
2017-06-01 17:59:08 -0400

    FINISH

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+(function main(){
+ 	var boxes = Array.from(document.getElementsByClassName('box-work'));
+ 	var modal = document.getElementById("box-work-modal");
+ 	var bodyModal, close, img;
+ 	boxes.forEach(function(box, i){
+ 		box.addEventListener("click", function(){
+ 			modal.innerHTML = "";
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;
+      
+      bodyModal.removeChild(bodyModal.childNodes[3]);
+ 			modal.appendChild(bodyModal);
+ 			modal.classList.remove("hide");
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			img = document.createElement("img");
+ 			img.setAttribute("src", "assets/images/close1.png");
+ 			close.appendChild(img);
+ 			modal.appendChild(close);
+ 			close.addEventListener("click",function(){
+ 				modal.classList.add("hide");
+ 			});
+ 		});
+ 	});
+ })();
